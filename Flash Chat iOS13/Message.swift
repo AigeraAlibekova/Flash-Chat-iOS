eb6a2d264d8b84347e73e901c5e5cfe962d95748
Aigera <aikoaliask@gmail.com>
2023-05-10 19:35:36 +0600

    first commit

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Aika Ali on 09.05.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
